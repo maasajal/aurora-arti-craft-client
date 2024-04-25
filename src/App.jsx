e10aef0ce8e4aae95e98ee473e77247771935d1f
Abdullah Sajal <59488317+sajal-se@users.noreply.github.com>
2024-04-26 02:24:27 +0300

    Choose & setup Painting and Drawing project for assignment 10

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="text-center py-5">
+        <h1 className="text-6xl ">Painting and Drawing</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
