89ce8290cfd9ba6e1fa2065ace23279f9af07efe
Abdullah Sajal <59488317+sajal-se@users.noreply.github.com>
2024-04-27 03:33:34 +0300

    Craft Items Section added with fake data

---
+++ src/components/CraftCard/CraftCard.jsx
@@ -0,0 +1,61 @@
+import { useContext } from "react";
+import { Link } from "react-router-dom";
+import { AuthContext } from "../../providers/AuthProvider";
+
+const CraftCard = ({ craft }) => {
+  const { loading } = useContext(AuthContext);
+  const {
+    image,
+    item_name,
+    subcategory_name,
+    short_description,
+    price,
+    rating,
+    customization,
+    stock_status,
+    user_name,
+  } = craft;
+  return (
+    <>
+      {!loading ? (
+        <div className="card card-compact bg-base-100 shadow-xl animate__animated animate__fadeInUpBig">
+          <div>
+            <img src={image} alt={item_name} />
+          </div>
+          <div className="card-body text-center">
+            <p className="flex justify-between">
+              <span>By: {user_name}</span> <span>{stock_status}</span>
+            </p>
+            <h2 className="text-3xl">{item_name} </h2>
+            <h3 className="text-lg font-semibold">
+              <span>Category: {subcategory_name}</span>{" "}
+            </h3>
+            <h3 className="text-lg font-semibold">
+              <span>Customization: {customization}</span>{" "}
+            </h3>
+            <p className="flex justify-between">
+              <span>Price: {price}</span> <span>Rating: {rating}</span>
+            </p>
+            <p className="mb-4 leading-8">{short_description}</p>
+            <div className="card-actions">
+              <Link
+                to={`/craft/${item_name}`}
+                className="btn px-6 border-none uppercase w-full text-white bg-gradient-to-r from-purple-700 via-pink-600 to-yellow-500"
+              >
+                View Details
+              </Link>
+            </div>
+          </div>
+        </div>
+      ) : (
+        <div>
+          <span className="loading loading-ring loading-xs"></span>
+          <span className="loading loading-ring loading-sm"></span>
+          <span className="loading loading-ring loading-md"></span>
+          <span className="loading loading-ring loading-lg"></span>
+        </div>
+      )}
+    </>
+  );
+};
+export default CraftCard;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,6 @@
 import { useLoaderData } from "react-router-dom";
 import Banner from "../../components/Banner/Banner";
+import CraftCard from "../../components/CraftCard/CraftCard";
 
 const Home = () => {
   const painting_drawing = useLoaderData();
@@ -9,12 +10,23 @@ const Home = () => {
       <div>
         <Banner sliderData={painting_drawing} />
       </div>
-      <div className="px-4 md:px-3 lg:px-12 my-12">
-        <h2 className="text-4xl font-playFair font-extrabold text-center">
-          <span className="bg-gradient-to-r from-purple-700 via-pink-600 to-yellow-500 text-transparent bg-clip-text">
-            Aurora Artify & Craft Emporium
-          </span>
-        </h2>
+      <div className="max-w-6xl mx-auto px-4 md:px-3 lg:px-12 my-12">
+        <div className="text-center py-8">
+          <h2 className="text-4xl font-playFair font-extrabold text-center">
+            <span className="bg-gradient-to-r from-purple-700 via-pink-600 to-yellow-500 text-transparent bg-clip-text">
+              Most Popular Craft Items
+            </span>
+          </h2>
+          <p className="mt-5 leading-8">
+            Explore our most popular craft items, curated for their quality,
+            creativity, and artisanal charm.
+          </p>
+        </div>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 my-24">
+          {painting_drawing.map((craft) => (
+            <CraftCard key={craft.id} craft={craft} />
+          ))}
+        </div>
       </div>
     </>
   );
