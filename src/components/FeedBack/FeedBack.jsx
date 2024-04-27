9f21a2a20a807cebc15dc3d87ce2cc9eb1040797
Abdullah Sajal <59488317+sajal-se@users.noreply.github.com>
2024-04-27 04:06:06 +0300

    Extera 2 sections added

---
+++ src/components/FeedBack/FeedBack.jsx
@@ -0,0 +1,99 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/effect-coverflow";
+import "swiper/css/pagination";
+
+// import required modules
+import { EffectCoverflow, Pagination, Autoplay } from "swiper/modules";
+import { useContext } from "react";
+import { AuthContext } from "../../providers/AuthProvider";
+
+const FeedBack = ({ feedback }) => {
+  const { loading } = useContext(AuthContext);
+  return (
+    <>
+      {!loading ? (
+        <Swiper
+          effect={"coverflow"}
+          grabCursor={true}
+          centeredSlides={true}
+          slidesPerView={3}
+          coverflowEffect={{
+            rotate: 50,
+            stretch: 0,
+            depth: 100,
+            modifier: 1,
+            slideShadows: true,
+          }}
+          autoplay={{
+            delay: 3000,
+            disableOnInteraction: false,
+          }}
+          pagination={true}
+          modules={[EffectCoverflow, Pagination, Autoplay]}
+          className="mySwiper"
+        >
+          {feedback.map((review) => (
+            <SwiperSlide key={review.id}>
+              <div className="card card-compact bg-base-100 shadow-xl">
+                <figure>
+                  <img
+                    src={review.image}
+                    alt={review.name}
+                    className="rounded-full w-1/2 p-5"
+                  />
+                </figure>
+                <div className="card-body text-center">
+                  <h2 className="text-3xl">{review.name}</h2>
+                  <p className="leading-8">{review.date}</p>
+                  <div className="font-semibold flex items-center flex-col gap-3">
+                    <p>Rating: {review.rating}</p>
+                    <div className="rating ">
+                      <input
+                        type="radio"
+                        name="rating-4"
+                        className="mask mask-star-2 bg-gradient-to-r from-purple-700 via-pink-600 to-yellow-500"
+                      />
+                      <input
+                        type="radio"
+                        name="rating-4"
+                        className="mask mask-star-2 bg-gradient-to-r from-purple-700 via-pink-600 to-yellow-500"
+                      />
+                      <input
+                        type="radio"
+                        name="rating-4"
+                        className="mask mask-star-2 bg-gradient-to-r from-purple-700 via-pink-600 to-yellow-500"
+                      />
+                      <input
+                        type="radio"
+                        name="rating-4"
+                        className="mask mask-star-2 bg-gradient-to-r from-purple-700 via-pink-600 to-yellow-500"
+                      />
+                      <input
+                        type="radio"
+                        name="rating-4"
+                        className="mask mask-star-2 bg-gradient-to-r from-purple-700 via-pink-600 to-yellow-500"
+                        defaultChecked
+                      />
+                    </div>
+                  </div>
+                  <q className="mb-4 leading-8">{review.comment}</q>
+                </div>
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      ) : (
+        <div>
+          <span className="loading loading-ring loading-xs"></span>
+          <span className="loading loading-ring loading-sm"></span>
+          <span className="loading loading-ring loading-md"></span>
+          <span className="loading loading-ring loading-lg"></span>
+        </div>
+      )}
+    </>
+  );
+};
+export default FeedBack;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,22 @@
 import { useLoaderData } from "react-router-dom";
 import Banner from "../../components/Banner/Banner";
 import CraftCard from "../../components/CraftCard/CraftCard";
+import { useEffect, useState } from "react";
+import FeedBack from "../../components/FeedBack/FeedBack";
 
 const Home = () => {
   const painting_drawing = useLoaderData();
-  console.log(painting_drawing);
+  const [feedback, setFeedback] = useState([]);
+  useEffect(() => {
+    const reviewData = async () => {
+      const res = await fetch("feedback.json");
+      const data = await res.json();
+      setFeedback(data);
+    };
+    return () => {
+      reviewData();
+    };
+  }, []);
   return (
     <>
       <div>
@@ -12,20 +24,62 @@ const Home = () => {
       </div>
       <div className="max-w-6xl mx-auto px-4 md:px-3 lg:px-12 my-12">
         <div className="text-center py-8">
-          <h2 className="text-4xl font-playFair font-extrabold text-center">
+          <h1 className="text-5xl font-playFair font-extrabold">
             <span className="bg-gradient-to-r from-purple-700 via-pink-600 to-yellow-500 text-transparent bg-clip-text">
               Most Popular Craft Items
             </span>
-          </h2>
+          </h1>
           <p className="mt-5 leading-8">
             Explore our most popular craft items, curated for their quality,
             creativity, and artisanal charm.
           </p>
         </div>
+        {/* Craft Items Section */}
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 my-24">
           {painting_drawing.map((craft) => (
             <CraftCard key={craft.id} craft={craft} />
           ))}
+        </div>
+        {/* Extra Section 1 */}
+        <div className="mt-32 text-center max-w-xl mx-auto">
+          <h2 className="text-5xl font-extrabold font-playFair">
+            <span className="bg-gradient-to-r from-purple-700 via-pink-600 to-yellow-500 text-transparent bg-clip-text">
+              What people say about us
+            </span>
+          </h2>
+          <p className="mt-5 leading-8">
+            Discover what our customers have to say about their delightful
+            experiences with us.
+          </p>
+        </div>
+        <div className="py-20 bg-slate-100 rounded-xl mt-8">
+          <FeedBack feedback={feedback} />
+        </div>
+        {/* Extra section 2 */}
+        <div
+          className="py-32 text-center bg-cover mt-28"
+          style={{
+            backgroundImage: "url(https://i.ibb.co/XztzdCV/2150981113.jpg)",
+          }}
+        >
+          <div className="stats shadow">
+            <div className="stat place-items-center">
+              <div className="stat-title">Total Sold</div>
+              <div className="stat-value">2599</div>
+            </div>
+            <div className="stat place-items-center">
+              <div className="stat-title">Social Media Reviews</div>
+              <div className="stat-value">2M</div>
+            </div>
+            <div className="stat place-items-center">
+              <div className="stat-title">Happy customers</div>
+              <div className="stat-value text-secondary">800</div>
+            </div>
+            <div className="stat place-items-center">
+              <div className="stat-title">Product variant</div>
+              <div className="stat-value">12</div>
+            </div>
+          </div>
         </div>
       </div>
     </>
